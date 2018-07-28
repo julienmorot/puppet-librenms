a455f7d30a42057aef0c20c0074cbf9e05920a81
Julien Morot <jmorot@gmail.com>
2018-07-28 09:03:47 +0200

    update mysql server conf and add librenms user

--- manifests/server.pp
+++ manifests/server.pp
@@ -4,8 +4,17 @@ class librenms::server {
     $pkgdep = ['acl','composer','fping','git','graphviz','imagemagick','mtr-tiny','nmap','python-memcache','python-mysqldb','rrdtool','snmp','snmpd','whois']
     package { $pkgdep: ensure => present }
 
+    $override_options = {
+        '[mysqld]' => {
+            innodb_file_per_table=>1,
+            sql-mode=>"",
+            lower_case_table_names=>0,
+        }
+    }
+
     class {'::mysql::server':
         root_password    => $::librenms::vars::dbrootpassword,
+        override_options        => $override_options
     }
 
     mysql::db { $::librenms::vars::dbname:
@@ -13,6 +22,14 @@ class librenms::server {
         password => $::librenms::vars::dbpassword,
         host     => $::librenms::vars::dbhost,
         grant    => ['ALL'],
+    }
+
+    user { 'librenms':
+        ensure     => 'present',
+        home       => '/opt/librenms',
+        managehome => true,
+        shell      => '/bin/bash',
+        groups     => 'www-data',
     }
 
 }
